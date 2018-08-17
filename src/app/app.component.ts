6cfcc4ae062cc95cfc5743d05cbe2f88897f9b3d
alejo <jamarin90@gmail.com>
2018-08-17 09:05:03 -0500

    se realiza el commit de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+
+  name: string = 'Jhon Cartes';
+  age: number = 28;
+  
+  users: string[] = ['ryan', 'joe', 'pedro'];
+  posts = [];
+
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data => {
+      console.log(data);
+      this.posts = data;
+    }
+    );
+  }
+
+  
+  
+  sayHello(){
+    alert('Hello!');
+
+  }
+  addUser(newUser){
+    this.users.push(newUser.value);
+    console.log(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+  
+  
+  deleteUser(user){
+    for(let i=0; i< this.users.length; i++){
+      if(user == this.users[i]){
+        this.users.splice(i, 1);
+
+      }
+    }
+
+  }
+  
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
+
+import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes:Route[] = [
+  {path: '', component: HolaMundoComponent},
+  {path: 'about', component: AboutComponent},
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundoComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
